6ac19db61ccfaa55e0777111b7904b742cdbecef
Omar Wasfi <alprincewasfi@gmail.com>
2019-06-28 11:43:58 +0200

    Initial DataModels Part 1

---
+++ W-SmartShopSelution/Library/DataModels/Humans/CustomerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataModels
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+
+        public PersonModel  Person { get; set; }
+
+        
+    }
+}
